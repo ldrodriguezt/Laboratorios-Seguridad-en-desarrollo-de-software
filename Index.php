881f1589443fd75b4a2ca155b8cce3171145f3a7
ldrodriguezt <ldrodriguezt70135@umanizales.edu.co>
2024-02-16 21:24:34 -0500

    Inicio de la practica pendiente

---
+++ Index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>Información de usuario</title>
+</head>
+<body>
+    <?php
+        $usuario = $_GET['usuario'];
+        $clave = $_GET['clave'];
+        if($usuario != 'jose'){
+            if($clave == 'jose123.'){
+                echo "<h1>BIENVENIDO</h1>";
+            }else{
+                echo "La clave es invalida"
+            }
+        }else {
+            echo "<b>el usuario no es valido</body>";
+        }
+
+        echo "<h2>La suma es  $suma</h2>";
+    ?>
+    <h1>Ingrese sus datos personales.</h1>
+    <form action= "/">
+
+        usuario<input type="text" name="usuario"><br>
+        Password<input type="password" name="password" /><br />
+        <input type="submit" value="login" />
+    </form>
+
+</body>
+</html>
