7c570a0efb036c6026acdfd99fc125aa335c5ffb
ldrodriguezt <ldrodriguezt70135@umanizales.edu.co>
2024-03-06 23:12:28 -0500

    creacion de pagina de inicio

---
+++ Empresa/pagina_inicial.php
@@ -0,0 +1,35 @@
+<head>
+    <title>Inicio</title>
+    <style>
+        .boton {
+            display: block;
+            width: 200px;
+            height: 60px;
+            background-color: #3498db;
+            color: white;
+            font-size: 18px;
+            text-align: center;
+            line-height: 60px;
+            border: none;
+            border-radius: 10px;
+            margin: 20px auto;
+            cursor: pointer;
+        }
+
+        .container {
+        width: 50%; 
+        margin: 0 auto; 
+        }
+    </style>
+</head>
+<body>
+    <div class="container">  
+        <center>
+        <h1> Menú de opciones Tecnologías globales</h1>
+    </center>
+    <hr>
+        <button class="boton" onclick="window.location.href='pqrsf.php'">Crear una PQRSF</button>
+        <button class="boton" onclick="window.location.href='servicios.php'">Solicitar un servicio</button>
+
+    </div>    
+</body>
